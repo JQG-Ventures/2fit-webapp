53cbac3a3849ef0ce7bcf90d85d89a1b9419f004
Gabriel <gabrielmorabena35@gmail.com>
2024-09-22 16:59:25 -0600

    i18n refactor added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,18 @@
+"use client"; // Asegúrate de agregar esta línea
+
 import "./globals.css";
 import "./_css/base.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { RegisterProvider } from "./_components/register/RegisterProvider";
 import { default as AuthProvider } from "./_providers/AuthProvider";
 import NavBar from "./_components/navbar/NavBar";
+import { LanguageProvider } from './utils/LanguageContext'; // Asegúrate de la ruta correcta
+import { ReactNode } from "react"; 
+import '@/app/utils/i18n'; // Importa la configuración de i18next
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "2Fit App",
-  description: "Embark on your new fitness journey",
-  // Additional meta tags can be added here
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <head>
@@ -22,13 +20,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;600&display=swap"></link>
       </head>
       <body id="test" className={inter.className}>
-        <RegisterProvider>
-          <AuthProvider>
-              {children}
-          </AuthProvider>
-          <NavBar />
-        </RegisterProvider>
+        <LanguageProvider>
+          <RegisterProvider>
+            {children}
+            <NavBar />
+          </RegisterProvider>
+        </LanguageProvider>
       </body>
     </html>
   );
 }
+
+export default RootLayout;
