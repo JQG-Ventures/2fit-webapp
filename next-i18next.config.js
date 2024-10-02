937b49c5c6805b59705ba8220c9c3218655d1977
Gabriel <gabrielmorabena35@gmail.com>
2024-10-01 19:04:55 -0600

    deleting comments and adding new translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'es'], // Asegúrate de que esto sea un array
+    locales: ['en', 'es'],
   },
-  localePath: path.resolve('./public/locals'), // Ruta a los archivos de traducción
+  localePath: path.resolve('./public/locals'),
 };
