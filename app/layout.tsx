e2b3a9d8eb6b136e9fdac39d63ab285e5aaf326a
JafetMo5601 <jafet.moraugalde15@gmail.com>
2024-10-05 15:16:25 -0600

    Fix layout dependencies and packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,28 +7,30 @@ import { RegisterProvider } from "./_components/register/RegisterProvider";
 import { default as AuthProvider } from "./_providers/AuthProvider";
 import NavBar from "./_components/navbar/NavBar";
 import { LanguageProvider } from './utils/LanguageContext';
-import { ReactNode } from "react"; 
-import '@/app/utils/i18n'; 
+import { ReactNode } from "react";
+import '@/app/utils/i18n';
 
 const inter = Inter({ subsets: ["latin"] });
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;600&display=swap"></link>
-      </head>
-      <body id="test" className={inter.className}>
-        <LanguageProvider>
-          <RegisterProvider>
-            {children}
-            <NavBar />
-          </RegisterProvider>
-        </LanguageProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="UTF-8" />
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;600&display=swap"></link>
+			</head>
+			<body id="test" className={inter.className}>
+				<LanguageProvider>
+					<RegisterProvider>
+						<AuthProvider>
+							{children}
+						</AuthProvider>
+						<NavBar />
+					</RegisterProvider>
+				</LanguageProvider>
+			</body>
+		</html>
+	);
 }
 
 export default RootLayout;
